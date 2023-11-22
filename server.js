effd1308cb615854dcce977dd94af12b73a242dd
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 15:52:00 +0000

    Use route with express use

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get("/api/contacts", (req, res) => {
-    res.status(200).json({ message: "Get all contacts" });
-})
+// app.get("/api/contacts", (req, res) => {
+//     res.status(200).json({ message: "Get all contacts" });
+// })
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
