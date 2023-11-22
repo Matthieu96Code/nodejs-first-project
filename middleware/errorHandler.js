2f8fd29dcf421c4f90ff8ee6b5b5f533c4741c76
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 18:34:16 +0000

    Add cases for constant error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,20 +1,40 @@
+const { constants } =require("../constants");
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch (statusCode) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({ 
                 title: "Validation Failed",
                 message: err.message,
                 stackTracer: errstack,
             });
             break;
-        case 404:
+        case constants.NOT_FOUND:
             res.json({ 
                 title: "Not Found",
                 message: err.message,
                 stackTracer: errstack,
             });
+        case constants.UNAUTHORIZED:
+            res.json({ 
+                title: "Unauthorized",
+                message: err.message,
+                stackTracer: errstack,
+            });
+        case constants.FORBIDDEN:
+            res.json({ 
+                title: "Forbidden",
+                message: err.message,
+                stackTracer: errstack,
+            });
+        case constants.SERVER_ERROR:
+            res.json({ 
+                title: "Server error",
+                message: err.message,
+                stackTracer: errstack,
+            });
         default:
+            console.log("No Error, All good !");
             break
     }
 };
