ca70b8e6cfea5ae9523920d787aac3aab5dc8068
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 17:09:49 +0000

    Add a contact controller and past all the logic

---
+++ controllers/contactController.js
@@ -0,0 +1,42 @@
+// @desc Get all contacts
+// @route GET /api/contacts
+// @access public
+const getContacts = (req, res) => {
+    res.status(200).json({ message: "Get all contacts" });
+};
+
+// @desc Create New contact
+// @route POST /api/contacts
+// @access public
+const createContact = (req, res) => {
+    res.status(201).json({ message: "Create Contact" });
+};
+
+// @desc Get contact
+// @route GET /api/contacts/:id
+// @access public
+const getContact = (req, res) => {
+    res.status(200).json({ message: `Get contact for ${req.params.id}` })
+};
+
+// @desc Update contact
+// @route PUT /api/contacts/:id
+// @access public
+const updateContact = (req, res) => {
+    res.status(200).json({ message: `Update contact for ${req.params.id}` })
+};
+
+// @desc Delete contact
+// @route DELETE /api/contacts/:id
+// @access public
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id}` })
+};
+
+module.exports = { 
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact,
+}; 
