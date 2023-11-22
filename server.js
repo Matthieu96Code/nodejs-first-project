15e1098b895579307a2d92c20de06bab81a007f9
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 13:48:14 +0000

    Show json message on status 200

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get("/api/contacts", (req, res) => {
+    res.status(200).json({ message: "Get all contacts" });
+})
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
