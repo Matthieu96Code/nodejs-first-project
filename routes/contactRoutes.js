9cb78dc4cded5ac848f87cdb9e1362fdc5f72f1c
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 16:37:28 +0000

    Create routes for all the crud operation

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,21 @@ router.route("/").get((req, res) => {
     res.status(200).json({ message: "Get all contacts" });
 });
 
+router.route("/").post((req, res) => {
+    res.status(200).json({ message: "Create contacts" })
+});
+
+router.route("/:id").get((req, res) => {
+    res.status(200).json({ message: `Get contact for ${req.params.id}` })
+});
+
+router.route("/:id").put((req, res) => {
+    res.status(200).json({ message: `Update contact for ${req.params.id}` })
+});
+
+router.route("/:id").delete((req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id}` })
+});
+
+
 module.exports = router;
