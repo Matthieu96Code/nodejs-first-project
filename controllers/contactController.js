0fd3d14aed8a8603317b5a43b6e502b752108d5d
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 18:34:59 +0000

    Use of async on the controller operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,14 +1,14 @@
 // @desc Get all contacts
 // @route GET /api/contacts
 // @access public
-const getContacts = (req, res) => {
+const getContacts = async (req, res) => {
     res.status(200).json({ message: "Get all contacts" });
 };
 
 // @desc Create New contact
 // @route POST /api/contacts
 // @access public
-const createContact = (req, res) => {
+const createContact = async (req, res) => {
     console.log("The request bbody is:", req.body);
     const { name, email, phone } = req.body;
     if (!name || !email || !phone) {
@@ -21,21 +21,21 @@ const createContact = (req, res) => {
 // @desc Get contact
 // @route GET /api/contacts/:id
 // @access public
-const getContact = (req, res) => {
+const getContact = async(req, res) => {
     res.status(200).json({ message: `Get contact for ${req.params.id}` })
 };
 
 // @desc Update contact
 // @route PUT /api/contacts/:id
 // @access public
-const updateContact = (req, res) => {
+const updateContact = async (req, res) => {
     res.status(200).json({ message: `Update contact for ${req.params.id}` })
 };
 
 // @desc Delete contact
 // @route DELETE /api/contacts/:id
 // @access public
-const deleteContact = (req, res) => {
+const deleteContact = async (req, res) => {
     res.status(200).json({ message: `Delete contact for ${req.params.id}` })
 };
 
