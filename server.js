ed6b2e60712002e5ed9b34128f0d4fbb9523af9f
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 13:24:03 +0000

    Import specific port from env

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
