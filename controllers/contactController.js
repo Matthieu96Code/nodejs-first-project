4ea373afab5dd37b9f47f0cbae3304f471d7b419
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 17:50:17 +0000

    Throw error for empty post parameters

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -10,6 +10,11 @@ const getContacts = (req, res) => {
 // @access public
 const createContact = (req, res) => {
     console.log("The request bbody is:", req.body);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory !");
+    }
     res.status(201).json({ message: "Create Contact" });
 };
 
