102133ac688f3885b8a677c6ac9490d02019c23a
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-22 17:14:28 +0000

    Contract routes definition

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -8,15 +8,7 @@ const {
     deleteContact, 
 } = require("../controllers/contactController") 
 
-router.route("/").get(getContacts);
-
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
-
+router.route("/").get(getContacts).post(createContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 module.exports = router;
