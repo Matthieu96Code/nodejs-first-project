2b4469f7190eb1764022a1096e1e016e72b3f0e4
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-23 17:06:20 +0000

    Implement login and current in user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 // @desc Register a user
@@ -39,14 +40,37 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "login user"});
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.statust(400);
+        throw new Error("All fields are mandatory");
+    }
+    const user = await User.findOne({ email });
+    // compare password with hashedpassword
+    if (user && (await bcrypt.compare(password, user.password))) {
+        const accessToken = jwt.sign(
+            {
+                user: {
+                    username: user.username,
+                    email: user.email,
+                    id: user.id
+                },
+            }, 
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: "1m" }
+        );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401);
+        throw new Error("email or pasword is not walid");
+    }
 });
 
 // @desc Current user info
 // @route POST /api/users/current
 // @access private
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Current user information"});
+    res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
