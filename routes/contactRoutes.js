56617681e3fcedd915dc8f639281b45f872cfc3c
Matthieu96Code <senoumatthieu96@gmail.com>
2023-11-23 17:55:31 +0000

    Upgrade validation time add validation to contact

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -6,8 +6,10 @@ const {
     getContact,
     updateContact,
     deleteContact, 
-} = require("../controllers/contactController") 
+} = require("../controllers/contactController"); 
+const validateToken = require("../middleware/validateTokenHandler");
 
+router.use(validateToken);
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
